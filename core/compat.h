92ebee674b8a1431663414cfa3345e1ff7407bc6
Chris Anderson <zsmyna@gmail.com>
2019-01-31 21:24:07 +1300

    Minor compatibility change for MSVC

--- core/compat.h
+++ core/compat.h
@@ -2,3 +2,11 @@
 
 #define CXX17_FALLTHROUGH [[fallthrough]]
 #define CXX17_NODISCARD [[nodiscard]]
+
+#ifdef _MSC_VER
+    #define VmCopyString(dest, src, count) strncpy_s(dest, count, src, count);
+    #define VmZeroMemory(dst,ch,destsz) memset_s(dst,ch,destsz,destsz)
+#else
+    #define VmCopyString(dest, src, count) strncpy(dest,src,count);
+    #define VmZeroMemory(dst,ch,destsz) memset(dst,ch,destsz)
+#endif
